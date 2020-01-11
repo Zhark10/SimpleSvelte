06b2194534aa454ea01682fa9c28899a72a10779
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-11 19:48:22 +0300

    added routing :sparkles:

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,18 @@
 <script>
-	export let name;
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./pages/Home.svelte";
+  import About from "./pages/About.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+  </nav>
+  <div>
+    <Route path="about" component="{About}" />
+    <Route path="/"><Home /></Route>
+  </div>
+</Router>
