4de2135ee4c5a4a630d4f3d969cf0e24f2fa347c
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-22 00:22:49 +0300

    added random people

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,8 +1,13 @@
 <script>
   let promise = getLuke();
-  
+
+  function getRandomId(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
   async function getLuke() {
-    let response = await fetch(`https://swapi.co/api/people/1/`);
+    let id = getRandomId(5);
+    let response = await fetch(`https://swapi.co/api/people/${id}/`);
     let data = await response.json();
     return data.name;
   }
