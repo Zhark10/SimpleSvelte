88e944b62708668d380334a7c7a5913ff387996d
Zhark10 <arkady.zharavin@citronium.biz>
2020-01-21 23:54:10 +0300

    added get request

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,2 +1,13 @@
+<script>
+  async function getUserAsync() {
+    let response = await fetch(`https://swapi.co/api/people/1/`);
+    let data = await response.json();
+    return data;
+  }
+
+  getUserAsync().then(data => console.log(data));
+</script>
+
 <h1>Home</h1>
 <p>Welcome to my website</p>
+
